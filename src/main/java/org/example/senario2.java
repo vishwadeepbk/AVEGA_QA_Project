6a079774fc88a7140351bb43cd1e8683cce0f485
vishwadeepbk <vishwadeepbk@gmail.com>
2026-07-31 11:41:46 +0530

    testing git

---
+++ src/main/java/org/example/senario2.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class senario2 {
+//    WebDriver driver = new ChromeDriver(); // Automatically downloads and links the driver binary
+//        driver.get("https://opensource-demo.orangehrmlive.com");
+//        Thread.sleep(10000);
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Admins");
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("admin123s");
+//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+//        Thread.sleep(10000);
+//        driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/div[1]/div[1]/div[1]/p")).getText().equals("Invalid credentials");
+
+
+}
